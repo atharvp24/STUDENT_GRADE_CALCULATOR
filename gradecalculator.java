af2d4cec13a6df728875a897ad0e4ef1ea284281
atharvp24 <atharvalokhande2022@gmail.com>
2024-07-19 22:16:15 +0530

    Add files via upload

---
+++ gradecalculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class gradecalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of subjects: ");
+        int numberOfSubjects = scanner.nextInt();
+        
+        int[] marks = new int[numberOfSubjects];
+        int totalMarks = 0;
+        
+        // Input marks for each subject
+        for (int i = 0; i < numberOfSubjects; i++) {
+            System.out.print("Enter the marks obtained in subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+            totalMarks += marks[i];
+        }
+        
+        // Calculate average percentage
+        double averagePercentage = (double) totalMarks / numberOfSubjects;
+        
+        // Determine the grade
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else if (averagePercentage >= 50) {
+            grade ='E';
+        }
+        else {
+            grade = 'F';
+        }
+        
+        // Display the results
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+        
+        scanner.close();
+    }
+}
